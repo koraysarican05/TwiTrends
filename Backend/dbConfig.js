92a228ccc9cf24ffb0ee32a0e00268861b8fdf42
Koray Sarıçan <koraysarican05@gmail.com>
2025-12-29 12:10:56 +0300

    Update dbConfig.js
    Remove hardcoded database credentials

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -1,13 +1,16 @@
 // TwiTrends-Backend/dbConfig.js
+require('dotenv').config();
+
 module.exports = {
-    user: 'sa',
-    password: '1234Koray!',
-    server: '127.0.0.1\\SQLEXPRESS',
-    port: 1433,
-    database: 'TwiTrends_1',
-    options: {
-      trustServerCertificate: true,
-      enableArithAbort: true
-    }
-  };
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  options: {
+    trustServerCertificate: true,
+    enableArithAbort: true
+  }
+};
+
   
